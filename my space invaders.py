07b39a940ae747b28f71a40ad22764f2c62c0913
JulieDeeley <JulieDeeley@users.noreply.github.com>
2018-07-07 23:38:12 +1200

    Add files via upload

--- my space invaders.py
+++ my space invaders.py
@@ -1,22 +1,16 @@
 import pygame
 from pygame.locals import *  # allows recognition of quit and x to quit
-import random
-import math
+
 
 # my Space Invaders July 7 2018
 
-pygame.init()
-clock = pygame.time.Clock()
+
+# === CONSTANTS === (UPPER_CASE names)
 
 # screen settings
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-
-#x_img = pygame.image.load('ship.png') example
-IMAGE_NAMES = ["ship"]
-IMAGES = {name: pygame.image.load(f'{name}.png')for name in IMAGE_NAMES}
-# ship sprite 24 X 30 px
+FPS = 30
 
 # Define some colors
 BLACK = (0, 0, 0)
@@ -27,48 +21,112 @@ BLUE = (10, 100, 255)
 LIGHT_BLUE = (100, 100, 255)
 LIGHT_RED = (255, 100, 100)
 LIGHT_GREEN = (100, 128, 100)
-my_ship = Ship([WIDTH / 2, HEIGHT / 2], [0, 0], 0, ship_image, ship_info)
 
-class Ship():
+# === LOAD IMAGES ===     x_img = pygame.image.load('ship.png') example
+
+IMAGE_NAMES = ["ship"]
+IMAGES = {name: pygame.image.load(f'{name}.png')for name in IMAGE_NAMES}
+# ship sprite 24 X 30 px
+
+
+# === CLASSES === (CamelCase names)
+
+class Player(pygame.sprite.Sprite):
+
     def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
         self.image = IMAGES['ship']
         self.rect = self.image.get_rect(topleft=(SCREEN_WIDTH//2-15, 540))
-        self.vel = 5
+        self.move_x = 0
+        self.move_y = 0
+        self.speed = 5
 
-    def draw(self):
-        screen.blit(self.image, self.rect)
+    def draw(self, surface):
+        surface.blit(self.image, self.rect)
 
     def update(self):
-        self.rect.x += self.vel
+        self.rect.x += self.move_x
+        self.rect.y += self.move_y
+
+    def event_handler(self, event):
+
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                self.move_x -= self.speed
+            elif event.key == pygame.K_RIGHT:
+                self.move_x += self.speed
+            elif event.key == pygame.K_UP:
+                self.move_y -= self.speed
+            elif event.key == pygame.K_DOWN:
+                self.move_y += self.speed
+
+        elif event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT:
+                self.move_x += self.speed
+            elif event.key == pygame.K_RIGHT:
+                self.move_x -= self.speed
+            elif event.key == pygame.K_UP:
+                self.move_y += self.speed
+            elif event.key == pygame.K_DOWN:
+                self.move_y -= self.speed
+
+# === MAIN === (lower_case names)
+
+# --- (global) variables ---
+
+# --- init ---
+
+pygame.init()
+clock = pygame.time.Clock()
+
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+screen_rect = screen.get_rect()
 
 
+# --- objects ---
 
-def main(self):
-    while True:
-        for event in pygame.event.get():
-            if event.type == QUIT or (
-                 event.type == KEYDOWN and (
-                  event.key == K_ESCAPE or
-                  event.key == K_q
-                 )):
-                pygame.quit()
-                quit()
-            elif event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_LEFT:
-                    self.rect.x  -=5
-                elif event.key == pygame.K_RIGHT:
-                    self.rect.x  +=5
+player = Player()
+
+# --- mainloop ---
+
+clock = pygame.time.Clock()
+is_running = True
+
+while is_running:
+
+    # --- events ---
+
+    for event in pygame.event.get():
+
+        # --- global events ---
+
+        if event.type == pygame.QUIT:
+            is_running = False
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_ESCAPE:
+                is_running = False
+
+        # --- objects events ---
+
+        player.event_handler(event)
+
+    # --- updates ---
+
+    player.update()
 
 
-        # User let up on a key
-            elif event.type == pygame.KEYUP:
-            # If it is an arrow key, reset vector back to zero
-                if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
-                    self.rect.x = 0
+    # --- draws ---
 
+    screen.fill(BLACK)
 
-        pygame.display.update()
-        clock.tick(60)  # sets fps to 60
+    player.draw(screen)
 
+    pygame.display.update()
 
-main()
+    # --- FPS ---
+
+    clock.tick(FPS)
+
+# --- the end ---
+
+pygame.quit()
